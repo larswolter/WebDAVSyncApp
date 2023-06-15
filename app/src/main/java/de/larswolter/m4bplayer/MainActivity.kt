ee4ce3269289cb294d43be06e9a23f8486b36378
Lars Wolter <lars@larswolter.de>
2023-06-15 23:19:39 +0200

    initial commit

---
+++ app/src/main/java/de/larswolter/m4bplayer/MainActivity.kt
@@ -0,0 +1,60 @@
+package de.larswolter.m4bplayer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+class MainActivity : ComponentActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      val navController = rememberNavController()
+      val viewModel = AudiobooksViewModel(application)
+      val list by viewModel.files.collectAsState()
+      val error by viewModel.error.collectAsState()
+      val status by viewModel.status.collectAsState()
+
+      MaterialTheme {
+        NavHost(navController = navController, startDestination = "list") {
+          composable("list") {
+            AppFrame(navController, audiobooks = viewModel, progress=status!="") {
+              Column {
+                Row(Modifier.padding(16.dp)) {
+                  Text(text="Status:")
+
+                  if(error!="") Text(text=error, color = Color.Red)
+                  if(status!="") {
+                    Text(text = status, color = Color.Gray, modifier=Modifier.weight(0.7f))
+                  }
+                }
+                AudiobookList(
+                  list = list,
+                )
+              }
+            }
+          }
+          composable("settings") {
+            AppFrame(navController) {
+              Settings(application)
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
